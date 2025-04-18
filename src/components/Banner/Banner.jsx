e6e5f952a659276f8a5776737783c6fe9ec92173
cosmokhairul <porosh.means.touch@gmail.com>
2025-04-18 08:39:29 +0600

    Second Commit

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="w-6/12 mx-auto">
+      <h1 className="text-red-500">Hello World</h1>
+    </div>
+  );
+};
+
+export default Banner;
--- src/components/Nabvar/Navbar.jsx
+++ src/components/Nabvar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="">
       <div className="navbar bg-base-100 shadow-sm">
         <div className="flex-1">
           <a className="btn btn-ghost text-xl">daisyUI</a>
